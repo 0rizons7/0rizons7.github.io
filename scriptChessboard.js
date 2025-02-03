0dde22ee64fce578b5e90405415b5328327b1afd
Orizons <orizons.dev@gmail.com>
2025-02-04 00:47:45 +0100

    04/02 Commit
    - Added evalutaion bar
    - Improved graphics features
    - Fixed issue where a piece couldn't move if it could check or checkmate.

--- scriptChessboard.js
+++ scriptChessboard.js
@@ -1,5 +1,15 @@
 import {Chess} from "./libs/chess.js";
 let game = new Chess();
+const castling = {
+    "O-O" : {
+        b : "g8",
+        w : "g1"
+    },
+    "O-O-O" : {
+        b : "c8",
+        w : "c1"
+    },
+}
 
 function onDragStart(source, piece, position, orientation) {
     if (game.game_over()) return false;
@@ -17,20 +27,20 @@ function onDrop(source, target) {
         promotion: 'q'
     });
     $('.square-legal').removeClass('square-legal')
+    $('.square-legal-take').removeClass('square-legal-take')
     if (move === null) return 'snapback';
 
     let history = game.history();
-    console.log(`${source}${target}`, history[history.length -1]);
+    console.log(history[history.length -1]);
 }
 
 function updateChessboard() {
     board.position(game.fen());
-    $('#fen').text(game.fen());
-    $('#pgn').text(game.pgn());
-    $('#pgn').scrollLeft($('#pgn')[0].scrollWidth);
+    $('#pgn').text(game.pgn()).scrollLeft($('#pgn')[0].scrollWidth);;
 
     let history = game.history({ verbose: true });
     history = history.map(move => move.from + move.to);
+    
     makeRequest(
         "https://explorer.lichess.ovh/masters",
         {
@@ -42,20 +52,44 @@ function updateChessboard() {
         data = JSON.parse(data);
         try {
             $('#openingName').html(data.opening.name.replace(":", ":<br>"))
-        } catch (error){
+        } catch (error) {
             console.error(error.message);
-            console.log(data.opening)
         }
-    }
-    ).fail(function (error) {
-            console.error(error);
-    });
+    })
+    makeRequest(
+        "https://lichess.org/api/cloud-eval",
+        {
+        fen: game.fen(),
+        },
+    ).done(function (data) {
+        data = JSON.parse(data);
+        try {
+            console.log(data.pvs[0].cp)
+            $('#whiteEval').css("height", `${(data.pvs[0].cp + 1000)/20}%`)
+            $('#eval').text(Math.abs(Math.round(data.pvs[0].cp/10)/10))
+            console.log($('#whiteEval').height())
+        } catch (error) {
+            console.error(error.message);
+        }
+    })
 }
 
 function renderMoveOptions(source) {
     let moves = game.moves({square:source});
+    if (moves[moves.length - 1] in castling) {
+        moves[moves.length - 1] = castling[moves[moves.length - 1]][game.turn()]
+    }
+    console.log(moves)
     moves.forEach(function (move) {
-        $('.square-'+move.slice(-2)).addClass('square-legal')
+        if (move.endsWith("#") || move.endsWith("+")) {
+            move = move.slice(0, -1)
+        }
+        if (move.includes("x")) {
+            $('.square-'+move.slice(-2)).addClass('square-legal-take')
+        } else {
+            $('.square-'+move.slice(-2)).addClass('square-legal')
+        }
+        
     });
 }
 
